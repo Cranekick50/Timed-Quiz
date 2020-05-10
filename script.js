b26c1be6e4f7176dc41569e5186a4879b8e454ec
Cranekick50 <michael.srithapin@gmail.com>
2020-05-09 19:45:28 -0500

    realized we need to get the timer working before displaying question

--- script.js
+++ script.js
@@ -3,34 +3,90 @@ var mainDiv = document.getElementById("main");
 var hsEl = document.getElementById("highScore");
 var timerEl = document.getElementById("timer");
 var startEl = document.getElementById("button");
-var secondsLeft = 120;
+var qTitleEl = document.getElementById("qTitle")
+var answerEl = document.querySelector("answerContainer");
+var secondsLeft = 60;
+
+var questionEl = document.getElementById("questionContainer");
+var answer1 = document.querySelector("answerOne")
+var answer2 = document.querySelector("answerTwo")
+var answer3 = document.querySelector("answerThree")
+var answer4 = document.querySelector("answerFour")
+
 // var scoreEl = document.
 
 //add styling to the page
-navDiv.style.color = "blue";
-navDiv.style.display = "flex";
-navDiv.style.justifyContent = "space-between";
-navDiv.style.padding = "25px";
+navDiv.setAttribute("style", "color:blue; display:flex; justify-content:space-between; padding:25px")
 
-mainDiv.style.padding = "250px";
+mainDiv.style.padding = "200px";
+
+//make a questions array
+let questions = [
+  {
+    question: "What does JS stand for?",
+    choiceA: "Juice Stand",
+    choiceB: "JavaStudio",
+    choiceC: "Jam Session",
+    choiceD: "JavaScript",
+    correct: "D"
+  },
+  {
+    question: "If you create a function in JS, what must you do to use it?",
+    choiceA: "Jumpstart the function",
+    choiceB: "Call the function",
+    choiceC: "PM the function",
+    choiceD: "Make the function constant",
+    correct: "B"
+  },
+  {
+    question: "What is an object that holds multiple values?",
+    choiceA: "Array",
+    choiceB: "Unordered List",
+    choiceC: "Alphanumeric",
+    choiceD: "Script Bundle",
+    correct: "A"
+  }
+]
 
 // add timer function to chain questions
 startEl.addEventListener("click", startTimer);
-timerEl.textContent = "Time:  " + secondsLeft;
-function startTimer() {
-      var timerInterval = setInterval(function () {
-      secondsLeft = secondsLeft - 1;
-    }, 1000);
 
-    startQ()
+function startTimer() {
+      startEl.style.display="none";
+      qTitleEl.style.display="none";
+      setInterval(function () {
+      secondsLeft = secondsLeft --;
+      timerEl.textContent="Time:  " + secondsLeft;
+    }, 1000);
+    
+    // renderQuestion()
   }
 
-
-// add end timer function to lose the game if out of time.
 // start button function must move to first question and start the timer
+let lastQuestionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+
+// function renderQuestion() {
+//     let q = question[runningQuestionIndex];
+//     question.innerHTML = "<p>"+q.question+"</p>"
+//     choiceA.innerHTML = q.chioiceA;
+//     choiceB.innerHTML = q.chioiceB;
+//     choiceC.innerHTML = q.chioiceC;
+//     choiceD.innerHTML = q.chioiceD;
+//     } 
+    
+
+// }
+// make an array of questions and answers
+// add end timer function to lose the game if out of time.
+
 // create function for right and wrong answers. create alert to reveal if right or wrong
 // create function to tally score and enter name
 // last page will be the list of top scores after name is submitted.
+// create an end game function if time runs out
+function endGame() {
+    mainDiv.textContent = "You failed :("
+}
 
 
 
